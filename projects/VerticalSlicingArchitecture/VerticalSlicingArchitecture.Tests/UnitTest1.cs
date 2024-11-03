35c7193ae6bcf156cb1917de2a5f5e44a60a946e
dmoka <mokabme@example.com>
2024-11-03 09:04:12 +0100

    add init test proj for VSA

---
+++ projects/VerticalSlicingArchitecture/VerticalSlicingArchitecture.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace VerticalSlicingArchitecture.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
