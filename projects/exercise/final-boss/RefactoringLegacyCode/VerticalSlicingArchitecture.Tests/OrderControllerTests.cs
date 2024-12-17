70006cab42e992524bf75d1f8da55172d3331509
dmoka <mokabme@example.com>
2024-12-17 16:41:49 +0100

    add final boss exercise

---
+++ projects/exercise/final-boss/RefactoringLegacyCode/VerticalSlicingArchitecture.Tests/OrderControllerTests.cs
@@ -0,0 +1,279 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text.Json;
+using FluentAssertions;
+using FsCheck;
+using Moq;
+using RefactoringLegacyCode.Tests.Asserters;
+using RefactoringLegacyCode.Tests.Shared;
+
+namespace RefactoringLegacyCode.Tests;
+
+public class OrderControllerTests
+{
+    [Test]
+    public async Task ProcessOrderShouldReturnProcessedOrderDetails()
+    {
+        using var testServer = new InMemoryServer();
+
+        testServer.DateTimeProviderMock.Setup(mock => mock.Now).Returns(new DateTime(2024, 12, 17, 8, 0, 0));
+        var response = await testServer.Client().PostAsync("api/order/1/process", null);
+
+
+        await HttpResponseAsserter.AssertThat(response).HasStatusCode(HttpStatusCode.OK);
+
+        await HttpResponseAsserter.AssertThat(response).HasJsonInBody(new
+        {
+            orderId = 1,
+            totalCost = 94.95,
+            estimatedDeliveryDate = new DateTime(2024, 12, 22, 8, 0, 0),
+            deliveryType = "Express"
+        });
+    }
+
+    [Test]
+    public async Task SendEmail_ShouldSendExpectedRequest()
+    {
+        // Arrange
+        var testServer = new InMemoryServer();
+
+        string calledPath = null;
+        StringContent capturedContent = null;
+
+        testServer.EmailSenderMock.Setup(sender => sender.SendEmailAsync(It.IsAny<string>(), It.IsAny<StringContent>()))
+            .Callback<string, StringContent>((path, content) =>
+            {
+                calledPath = path;
+                capturedContent = content;
+            });
+
+        // Act  
+        var response = await testServer.Client().PostAsync("api/order/1/process", null);
+
+        // Assert
+        await HttpResponseAsserter.AssertThat(response).HasStatusCode(HttpStatusCode.OK);
+
+        var actualEmailPayloadJson = await capturedContent.ReadAsStringAsync();
+        var actualEmailPayload = JsonSerializer.Deserialize<Dictionary<string, string>>(actualEmailPayloadJson);
+
+        calledPath.Should().Be("https://api.emailservice.com/send");
+        actualEmailPayload["to"].Should().Be("customer@example.com");
+        actualEmailPayload["subject"].Should().Be($"Order Confirmation - Order #1");
+        actualEmailPayload["body"].Should().Be($"Dear Customer,\n\nThank you for your order #1. Your order has been processed and will be delivered soon.\n\nBest Regards,\nWarehouse Team");
+        capturedContent.Headers.ContentType.MediaType.Should().Be("application/json");
+        capturedContent.Headers.ContentType.CharSet.Should().Be("utf-8");
+    }
+
+    [Test]
+    public async Task ShouldSaveOrderProcessingInXml()
+    {
+        using var testServer = new InMemoryServer();
+
+        testServer.DateTimeProviderMock.Setup(mock => mock.Now).Returns(new DateTime(2024, 12, 17, 8, 0, 0));
+        var response = await testServer.Client().PostAsync("api/order/1/process", null);
+
+        var xmlPath = Path.Combine(Environment.CurrentDirectory, $"Order_1.xml");
+        
+        await VerifyFile(xmlPath);
+    }
+
+    [TestCase("Express", 18, 10, 70)]
+    [TestCase("Express", 18, 11, 150)]
+    [TestCase("Express", 17, 11, 120)]
+    [TestCase("SameDay", 11, 11, 180)]
+    [TestCase("SameDay", 13, 11, 160)]
+    [TestCase("SameDay", 12, 11, 160)]
+    [TestCase("SameDay", 13, 11, 160)]
+    [TestCase("Standard", 13, 51, 100)]
+    [TestCase("Standard", 13, 50, 80)]
+    [TestCase("Express", 18, 9, 70)]
+    [TestCase("Express", 17, 9, 60)]
+    [TestCase("Express", 17, 5, 50)]
+    [TestCase("SameDay", 11, 9, 90)]
+    [TestCase("SameDay", 12, 9, 110)]
+    [TestCase("Standard", 12, 9, 20)]
+    [TestCase("Standard", 18, 9, 40)]
+    public void CharacterizePriorityCalculation(string deliverType, int hour, int quantity, int expectedPriority)
+    {
+        var dateTime = new DateTime(2024, 12, 17, hour, 0, 0);
+        var orderDetails = new OrderDetails()
+        {
+            DeliveryType = deliverType,
+            Quantity = quantity
+        };
+        var priority = OrderController.CalculatePriority(orderDetails, dateTime);
+
+        priority.Should().Be(expectedPriority);
+    }
+
+    public static Arbitrary<int> Hours()
+    {
+        return Arb.From(Gen.Choose(1, 23));
+    }
+
+    public static Arbitrary<int> Quantities()
+    {
+        return Arb.From(Gen.Choose(1, 100));
+    }
+
+    public static Arbitrary<string> DeliveryTypes()
+    {
+        return Arb.From(Gen.Elements("Express", "SameDay", "Standard"));
+    }
+
+    [Test]
+    public void HigherQuantity_shouldLeadHigherPriority()
+    {
+        Prop.ForAll(DeliveryTypes(), Hours(), Quantities(), (deliveryType, hour, quantity) =>
+        {
+            var dateTime = new DateTime(2024, 12, 17, hour, 0, 0);
+            var orderDetails = new OrderDetails()
+            {
+                DeliveryType = deliveryType,
+                Quantity = quantity
+            };
+            var priority = OrderController.CalculatePriority(orderDetails, dateTime);
+
+            orderDetails = new OrderDetails()
+            {
+                DeliveryType = deliveryType,
+                Quantity = quantity + 1
+            };
+            var higherPriority = OrderController.CalculatePriority(orderDetails, dateTime);
+
+            return higherPriority >= priority;
+
+        }).VerboseCheckThrowOnFailure();
+    }
+
+    [Test]
+    public void PriorityOrderShouldBe_SameDay_Then_Express_Then_Standard()
+    {
+        Configuration.Default.MaxNbOfTest = 100;
+
+        Prop.ForAll(
+            Quantities(),
+            Hours(),
+            (quantity, hour) =>
+            {
+                var dateTime = new DateTime(2024, 12, 17, hour, 0, 0);
+
+                var orderDetails = new OrderDetails
+                {
+                    Quantity = quantity,
+                    DeliveryType = "SameDay"
+                };
+                var sameDayPriority = OrderController.CalculatePriority(orderDetails, dateTime);
+
+                orderDetails = new OrderDetails
+                {
+                    Quantity = quantity,
+                    DeliveryType = "Express"
+                };
+                var expressPriority = OrderController.CalculatePriority(orderDetails, dateTime);
+
+                orderDetails = new OrderDetails
+                {
+                    Quantity = quantity,
+                    DeliveryType = "Standard"
+                };
+                var standardPriority = OrderController.CalculatePriority(orderDetails, dateTime);
+
+
+                return sameDayPriority > expressPriority && expressPriority > standardPriority;
+            }
+        ).VerboseCheckThrowOnFailure();
+    }
+
+    [Test]
+    public async Task SuccessfullProcessingShouldMarkOrderProcessed()
+    {
+        //Arrange
+        var testServer = new InMemoryServer();
+        testServer.DateTimeProviderMock.Setup(provider => provider.Now).Returns(new DateTime(2024, 11, 7, 10, 10, 10));
+
+        var state = testServer.GetOrderState(1);
+        state.Should().Be("New");
+
+        //Act
+        var response = await testServer.Client().PostAsync("api/order/1/process", null);
+
+        //Assert
+        var newState = testServer.GetOrderState(1);
+        newState.Should().Be("Processed");
+    }
+
+    [Test]
+    public async Task ProcessingShouldBadRequestWhenNonExistingOrderPassed()
+    {
+        //Arrange
+        var testServer = new InMemoryServer();
+        testServer.DateTimeProviderMock.Setup(provider => provider.Now).Returns(new DateTime(2024, 11, 7, 10, 10, 10));
+
+        var state = testServer.GetOrderState(1);
+        state.Should().Be("New");
+
+        //Act
+        var response = await testServer.Client().PostAsync("api/order/99/process", null);
+
+        //Assert
+        await HttpResponseAsserter.AssertThat(response).HasStatusCode(HttpStatusCode.BadRequest);
+        await HttpResponseAsserter.AssertThat(response).HasTextInBody("Order not found.");
+    }
+
+    [Test]
+    public async Task ShouldProcessOrderWhenUsedAllStockAviable()
+    {
+        using var testServer = new InMemoryServer();
+
+        testServer.InsertOrder(2, 200, 5);
+        testServer.InsertProduct(200, 5, 20);
+        testServer.DateTimeProviderMock.Setup(mock => mock.Now).Returns(new DateTime(2024, 12, 17, 8, 0, 0));
+
+        var response = await testServer.Client().PostAsync("api/order/2/process", null);
+
+        await HttpResponseAsserter.AssertThat(response).HasStatusCode(HttpStatusCode.OK);
+
+        var stockLevel = testServer.GetStockLevel(200);
+        stockLevel.Should().Be(0);
+    }
+
+    [Test]
+    public async Task ShouldFailWhenStockNotAvaiable()
+    {
+        using var testServer = new InMemoryServer();
+
+        testServer.InsertOrder(2, 200, 5);
+        testServer.InsertProduct(200, 4, 20);
+        testServer.DateTimeProviderMock.Setup(mock => mock.Now).Returns(new DateTime(2024, 12, 17, 8, 0, 0));
+
+        var response = await testServer.Client().PostAsync("api/order/2/process", null);
+
+        await HttpResponseAsserter.AssertThat(response).HasStatusCode(HttpStatusCode.BadRequest);
+        await HttpResponseAsserter.AssertThat(response).HasTextInBody("Insufficient stock to process the order.");
+    }
+
+    [Test]
+    public async Task ShouldReturnDifferentDeliverDateWhenQuantityIsBiggerThanFive()
+    {
+        using var testServer = new InMemoryServer();
+
+        testServer.InsertOrder(2, 200, 6);
+        testServer.InsertProduct(200, 6, 20);
+        testServer.DateTimeProviderMock.Setup(mock => mock.Now).Returns(new DateTime(2024, 12, 17, 8, 0, 0));
+
+        var response = await testServer.Client().PostAsync("api/order/2/process", null);
+
+        await HttpResponseAsserter.AssertThat(response).HasStatusCode(HttpStatusCode.OK);
+
+        await HttpResponseAsserter.AssertThat(response).HasJsonInBody(new
+        {
+            orderId = 2,
+            totalCost = 120,
+            estimatedDeliveryDate = new DateTime(2024, 12, 19, 8, 0, 0),
+            deliveryType = "Express"
+        });
+    }
+
+}
---
+++ projects/exercise/final-boss/RefactoringLegacyCode/VerticalSlicingArchitecture.Tests/Shared/InMemoryServer.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Data.SqlClient;
+using Moq.Protected;
+using Moq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.EntityFrameworkCore;
+using RefactoringLegacyCode.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace RefactoringLegacyCode.Tests.Shared
+{
+    public class InMemoryServer : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        private HttpClient _client;
+
+        public string ConnectionString => _connection.ConnectionString;
+
+        public Mock<IEmailSender> EmailSenderMock = new Mock<IEmailSender>();
+        public Mock<IDateTimeProvider> DateTimeProviderMock = new Mock<IDateTimeProvider>();
+
+        public InMemoryServer()
+        {
+            // Create and open the SQLite in-memory database
+            SQLitePCL.Batteries.Init();
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Testing");
+
+            var uniqueDbId = Guid.NewGuid().ToString();
+            var dataSource = $"DataSource=file:{uniqueDbId}?mode=memory&cache=shared";
+            _connection = new SqliteConnection(dataSource);
+            _connection.Open();
+            InitializeDatabase();
+            Setup();
+        }
+
+        private void InitializeDatabase()
+        {
+            using var command = _connection.CreateCommand();
+            command.CommandText = @"
+            CREATE TABLE Orders (
+                Id INTEGER PRIMARY KEY,
+                ProductId INTEGER,
+                Quantity INTEGER,
+                CustomerEmail TEXT,
+                DeliveryType TEXT,
+                Status TEXT DEFAULT 'New'
+            );
+
+            CREATE TABLE Products (
+                Id INTEGER PRIMARY KEY,
+                Quantity INTEGER,
+                Price NUMERIC(10, 2)
+            );
+
+            INSERT INTO Orders (Id, ProductId, Quantity, CustomerEmail, DeliveryType)
+            VALUES (1, 100, 5, 'customer@example.com', 'Express');
+
+            INSERT INTO Products (Id, Quantity, Price)
+            VALUES (100, 10, 18.99);
+        ";
+            command.ExecuteNonQuery();
+
+            var orders = GetOrders();
+        }
+
+        public string GetOrderState(int id)
+        {
+            var command = new SqliteCommand("SELECT Status FROM Orders WHERE Id = @Id", _connection);
+            command.Parameters.AddWithValue("@Id", id);
+
+            return (string)command.ExecuteScalar();
+        }
+
+        public void InsertOrder(int id, int productId, int quantity)
+        {
+            using var command = _connection.CreateCommand();
+            command.CommandText = $@"
+            INSERT INTO Orders (Id, ProductId, Quantity, CustomerEmail, DeliveryType)
+            VALUES ({id}, {productId}, {quantity}, 'customer@example.com', 'Express');
+        ";
+            command.ExecuteNonQuery();
+        }
+
+        public void InsertProduct(int productId, int quantity, decimal price)
+        {
+            using var command = _connection.CreateCommand();
+            command.CommandText = $@"
+            INSERT INTO Products (Id, Quantity, Price)
+            VALUES ({productId}, {quantity}, {price});
+        ";
+            command.ExecuteNonQuery();
+        }
+
+        public int GetStockLevel(int productId)
+        {
+            var command = new SqliteCommand("SELECT Quantity FROM Products WHERE Id = @ProductId", _connection);
+            command.Parameters.AddWithValue("@ProductId", productId);
+
+            return (int)(long)command.ExecuteScalar();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        public HttpClient Client() => _client;
+
+        public void Setup()
+        {
+            var factory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureLogging(logging =>
+                    {
+                        // Set minimal logging level
+                        logging.ClearProviders(); // Remove default providers
+                        logging.AddConsole(); // Optional: Add console logging if needed
+                        logging.SetMinimumLevel(LogLevel.Warning); // Show only warnings and errors
+                    });
+
+                    // Disable HTTPS redirection
+
+                    builder.ConfigureServices(services =>
+                    {
+
+                        // Override the connection string for tests
+                        services.AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                            .AddInMemoryCollection(new Dictionary<string, string>
+                            {
+                            { "ConnectionStrings:DefaultConnection", _connection.ConnectionString }
+                            })
+                            .Build());
+
+                        services.AddDbContext<WarehousingDbContext>(options => options.UseSqlite(_connection));
+                        services.AddSingleton(EmailSenderMock.Object);
+                        services.AddSingleton(DateTimeProviderMock.Object);
+
+                        services.Configure<HttpsRedirectionOptions>(options =>
+                        {
+                            options.HttpsPort = 443;  // Set a dummy port or disable redirection
+                        });
+                    });
+                });
+
+            var client =  factory.CreateClient();
+            _client = client;
+        }
+
+        public List<string> GetOrders()
+        {
+            var orders = new List<string>();
+
+            using var command = _connection.CreateCommand();
+            command.CommandText = "SELECT Id, ProductId, Quantity, CustomerEmail FROM Orders";
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var orderDetails = $"Id: {reader.GetInt32(0)}, ProductId: {reader.GetInt32(1)}, Quantity: {reader.GetInt32(2)}, Email: {reader.GetString(3)}";
+                orders.Add(orderDetails);
+            }
+
+            return orders;
+        }
+    }
+
+}
