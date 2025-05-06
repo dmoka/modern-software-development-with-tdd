ef5320d3806d4b345dd0af45620d6fbb80fef911
dmoka <mokabme@example.com>
2025-05-06 19:23:35 +0200

    remove final boss

--- projects/exercise/final-boss/RefactoringLegacyCode/VerticalSlicingArchitecture/Shared/Error.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VerticalSlicingArchitecture.Shared
-{
-    public sealed record Error(string Code, string Description)
-    {
-        public static readonly Error Empty = new(string.Empty, string.Empty);
-    }
-}
